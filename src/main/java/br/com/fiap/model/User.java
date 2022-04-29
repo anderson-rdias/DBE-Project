0e4fe1c688fc0829734662eebfeca980572d1954
Logon Aluno <logonrmlocal@fiap.com.br>
2022-04-29 10:11:43 -0300

    checkpoint

---
+++ src/main/java/br/com/fiap/model/User.java
@@ -0,0 +1,55 @@
+package br.com.fiap.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome = "Digite seu nome: ";
+	private LocalDate dtNascimento;
+	private String email = "Digite seu E-mail: ";
+	private String senha = "Digite sua senha: ";
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public LocalDate getDtNascimento() {
+		return dtNascimento;
+	}
+	public void setDtNascimento(LocalDate dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [nome=" + nome + ", data de nascimento =" + dtNascimento + ", email=" + email + ", senha=" + senha + "]";
+	}
+	
+
+}

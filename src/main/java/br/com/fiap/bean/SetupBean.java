a73fbfb33db72d1d2207acb6e5d7c8befbdf5689
Anderson Rodrigues Dias <anderson33rd@hotmail.com>
2022-05-06 09:54:46 -0300

    Aula 06-05

--- src/main/java/br/com/fiap/bean/SetupBean.java
+++ src/main/java/br/com/fiap/bean/SetupBean.java
@@ -1,9 +1,16 @@
 package br.com.fiap.bean;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.ServletContext;
+
+import org.primefaces.model.file.UploadedFile;
 
 import br.com.fiap.dao.SetupDao;
 import br.com.fiap.model.Setup;
@@ -11,12 +18,41 @@ import br.com.fiap.model.Setup;
 @Named
 @RequestScoped
 public class SetupBean {
+	
+	private UploadedFile image;
 
 	private Setup setup = new Setup();
 	
-	public void save() {
-		System.out.println(this.setup);
+	public String save() throws IOException {
+		
+		ServletContext servletContext = (ServletContext) FacesContext
+													.getCurrentInstance()
+													.getExternalContext()
+													.getContext();
+		
+		String path = servletContext.getRealPath("/");
+		
+		FileOutputStream out = new FileOutputStream(path + "\\images\\setups\\" + image.getFileName());
+		out.write(image.getContent());
+		out.close();
+		
+		setup.setImagePath("\\images\\setups\\" + image.getFileName());
+		
 		new SetupDao().insert(setup);
+		
+		mostrarMensagem();
+		
+		return "setups?faces-redirect=true";
+	}
+
+	private void mostrarMensagem() {
+		FacesContext.getCurrentInstance()
+		.getExternalContext()
+		.getFlash()
+		.setKeepMessages(true);
+		
+		FacesContext.getCurrentInstance()
+		.addMessage(null, new FacesMessage("Setup cadastrado!"));
 	}
 	
 	public List<Setup> getSetups(){
@@ -29,6 +65,14 @@ public class SetupBean {
 
 	public void setSetup(Setup setup) {
 		this.setup = setup;
+	}
+	
+	public UploadedFile getImage() {
+		return image;
+	}
+
+	public void setImage(UploadedFile image) {
+		this.image = image;
 	}
 
 }
